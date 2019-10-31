7d01e3b6b3766aae806df79f0d508428ef53e9b9
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2019-10-31 21:57:28 +0100

    web interface dev setup

--- src/interfaces/web/server.ts
+++ src/interfaces/web/server.ts
@@ -5,7 +5,10 @@ import * as path from "path"
 
 const app = express();
 
-console.log("asd");
+function sendFile(res, p) {
+  res.sendFile(path.join(__dirname, "../../../src/interfaces/web", p));
+}
+
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,8 +16,14 @@ app.use(bodyParser.json());
 
 
 
-app.use(express.static("/"));
+app.use("/dist", express.static("dist"));
 
-app.get("/", (req, res) => {
-  
+app.get('*', (req, res) => {
+  sendFile(res, "./view/index.html")
+});
+
+
+app.listen(1000, (err) => {
+  if (err) console.log(err);
+  else console.log("Listening on 1000");
 })
